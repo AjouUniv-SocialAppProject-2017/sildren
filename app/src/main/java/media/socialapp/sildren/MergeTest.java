073c45bec1eace032ca21bdc6a6b7aa45ff1dd4c
RYUUUUUUU <wjdgus392@ajou.ac.kr>
2017-11-06 17:39:54 +0900

    abc

--- app/src/main/java/media/socialapp/sildren/MergeTest.java
+++ app/src/main/java/media/socialapp/sildren/MergeTest.java
@@ -1,14 +1,11 @@
 package media.socialapp.sildren;
 
 /**
- * Created by andong-won on 2017. 11. 6..
+ * Edited by Jeonghyeon on 2017. 11. 6..
+
  */
 
 public class MergeTest {
 
     int test1;
-    int test2;
-    int test3;
-    int test4;
-    int test5;
 }
